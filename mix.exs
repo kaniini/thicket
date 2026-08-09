a15b39f823ca9c343ec01b489361aa7d7f804a82
Ariadne Conill <ariadne@ariadne.space>
2026-08-09 02:07:46 -0700

    fix(dev): restore Phoenix code reload listener

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Thicket.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      compilers: [:phoenix_live_view] ++ Mix.compilers()
+      compilers: [:phoenix_live_view] ++ Mix.compilers(),
+      listeners: [Phoenix.CodeReloader]
     ]
   end
 
