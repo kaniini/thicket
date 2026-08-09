9c90c36289952156c03e9f90a2ab901004b002f7
Ariadne Conill <ariadne@ariadne.space>
2026-08-09 01:36:33 -0700

    chore(ops): add release and health check skeleton

---
+++ lib/thicket_web/controllers/health_controller.ex
@@ -0,0 +1,12 @@
+defmodule ThicketWeb.HealthController do
+  use ThicketWeb, :controller
+
+  def live(conn, _params), do: json(conn, %{status: "ok"})
+
+  def ready(conn, _params) do
+    case Ecto.Adapters.SQL.query(Thicket.Repo, "SELECT 1", []) do
+      {:ok, _} -> json(conn, %{status: "ready", database: "ok"})
+      {:error, _} -> conn |> put_status(:service_unavailable) |> json(%{status: "unavailable", database: "error"})
+    end
+  end
+end
